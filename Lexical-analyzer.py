326dc54660afc3950dcabc03dec61d34db82a991
alireza reisi <alireza.reisi03127@gmail.com>
2023-11-20 02:43:19 +0330

    conditions set exept the last condition of cheking var , with out any run or debug

--- Lexical-analyzer.py
+++ Lexical-analyzer.py
@@ -1,177 +1,379 @@
 def GetToken(Lexim):
-    c=0
+    case=0
     i=0
-    match c:
-        case 0:
-            if(Lexim[i]=="1"):
-                i+=1
-                case="var1"
-            elif(Lexim[i]=="p"):
-                pass
-            elif(Lexim[i]=="b"):
-                pass
-            elif(Lexim[i]=="j"):
-                pass
-            elif(Lexim[i]=="n"):
-                pass
-            elif(Lexim[i]=="d"):
-                pass
-            elif(Lexim[i]=="e"):
-                pass
-            else:
+    while True:
+        match case:
+            case 0:
+                if(Lexim[i]=="1"):
+                    i+=1
+                    case="var1"
+                elif(Lexim[i]=="p"):
+                    i+=1
+                    case="if1"
+                elif(Lexim[i]=="b"):
+                    i+=1
+                    case="else_if1"
+                elif(Lexim[i]=="j"):
+                    i+=1
+                    case="else1"
+                elif(Lexim[i]=="n"):
+                    i+=1
+                    case="for1"
+                elif(Lexim[i]=="d"):
+                    i+=1
+                    case="d_checker"
+                elif(Lexim[i]=="e"):
+                    i+=1
+                    case="output1"
+                else:
+                    case="operations"
+            # ----------------------------
+            
+            case "d_checker":
+                if (Lexim[i]=="a"):
+                    i=i+1
+                    case="def1"
+                elif (Lexim[i]=="o"):
+                    i=i+1
+                    case="input1"
+                else:
+                    return "EROR"
+            
+            # ----------------------------
+            
+            case "var1":
+                if(Lexim[i]=="5"):
+                    i+=1
+                    case="var2"
+                else:
+                    return "EROR"
+            case "var2":
+                if(Lexim[i]=="_"):
+                    i+=1
+                    case="var3"
+                else:
+                    return "EROR"
+            case "var3":
                 pass
             
-        case "var1":
-            pass
-        case "var2":
-            pass
-        case "var3":
-            pass
-        
-        # ----------------------------
-        
-        case "if1":
-            pass
-        case "if2":
-            pass
-        case "if3":
-            pass
-        case "if4":
-            pass
-        case "if5":
-            pass
-        case "if6":
-            pass
-        case "if7":
-            pass
-        case "if8":
-            pass
-        case "if9":
-            pass
-        
-        # ----------------------------
+            # ----------------------------
             
-        case "else_if1":
-            pass
-        case "else_if2":
-            pass
-        case "else_if3":
-            pass
-        case "else_if4":
-            pass
-        case "else_if5":
-            pass
-        case "else_if6":
-            pass
-        case "else_if7":
-            pass
-        
-        # ----------------------------
-        
-        case "else1":
-            pass
-        case "else2":
-            pass
-        case "else3":
-            pass
-        case "else4":
-            pass
-        case "else5":
-            pass
-        case "else6":
-            pass
-        case "else7":
-            pass
-        
-        # ----------------------------
-        
-        case "for1":
-            pass
-        case "for2":
-            pass
-        case "for3":
-            pass
-        case "for4":
-            pass
-        case "for5":
-            pass
-        
-        # ----------------------------
-        
-        case "def1":
-            pass
-        case "def2":
-            pass
-        case "def3":
-            pass
-        case "def4":
-            pass
-        case "def5":
-            pass
-        case "def6":
-            pass
-        case "def7":
-            pass
-        case "def8":
-            pass
-        case "def9":
-            pass
-        case "def10":
-            pass
-        case "def11":
-            pass
-        case "def12":
-            pass
-        case "def13":
-            pass
-        case "def14":
-            pass
-        case "def15":
-            pass
+            case "if1":
+                if(Lexim[i]=="o"):
+                    i+=1
+                    case="if2"
+                else:
+                    return "EROR"
+            case "if2":
+                if(Lexim[i]=="l"):
+                    i+=1
+                    case="if3"
+                else:
+                    return "EROR"
+            case "if3":
+                if(Lexim[i]=="_"):
+                    i+=1
+                    case="if4"
+                else:
+                    return "EROR"
+            case "if4":
+                if(Lexim[i]=="s"):
+                    i+=1
+                    case="if5"
+                else:
+                    return "EROR"
+            case "if5":
+                if(Lexim[i]=="e"):
+                    i+=1
+                    case="if6"
+                else:
+                    return "EROR"
+            case "if6":
+                if(Lexim[i]=="r"):
+                    i+=1
+                    case="if7"
+                else:
+                    return "EROR"
+            case "if7":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="if8"
+                else:
+                    return "EROR"
+            case "if8":
+                if(Lexim[i]=="t"):
+                    i+=1
+                    case="if9"
+                else:
+                    return "EROR"
+            case "if9":
+                if(Lexim[i]=="\n"):
+                    return "<IfToken>"
+                else:
+                    return "EROR"
             
-        # ----------------------------
-        
-        case "call1":
-            pass
-        case "call2":
-            pass
-        case "call3":
-            pass
-        case "call4":
-            pass
-        case "call5":
-            pass
-        case "call6":
-            pass
-        case "call7":
-            pass
-        
-        # ----------------------------
-        
-        case "input1":
-            pass
-        case "input2":
-            pass
-        case "input3":
-            pass
-        
-        # ----------------------------
-        
-        case "output1":
-            pass
-        case "output2":
-            pass
-        case "output3":
-            pass
-        case "output4":
-            pass
-        case "output5":
-            pass
-        case "output6":
-            pass
-        
-        # ----------------------------
-        
+            # ----------------------------
+                
+            case "else_if1":
+                if(Lexim[i]=="e"):
+                    i+=1
+                    case="else_if2"
+                else:
+                    return "EROR"
+            case "else_if2":
+                if(Lexim[i]=="h"):
+                    i+=1
+                    case="else_if3"
+                else:
+                    return "EROR"
+            case "else_if3":
+                if(Lexim[i]=="e"):
+                    i+=1
+                    case="else_if4"
+                else:
+                    return "EROR"
+            case "else_if4":
+                if(Lexim[i]=="s"):
+                    i+=1
+                    case="else_if5"
+                else:
+                    return "EROR"
+            case "else_if5":
+                if(Lexim[i]=="h"):
+                    i+=1
+                    case="else_if6"
+                else:
+                    return "EROR"
+            case "else_if6":
+                if(Lexim[i]=="t"):
+                    i+=1
+                    case="else_if7"
+                else:
+                    return "EROR"
+            case "else_if7":
+                if(Lexim[i]=="\n"):
+                    return "<ElseifToken>"
+                else:
+                    return "EROR"
+            
+            # ----------------------------
+            
+            case "else1":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="else2"
+                else:
+                    return "EROR"
+            case "else2":
+                if(Lexim[i]=="h"):
+                    i+=1
+                    case="else3"
+                else:
+                    return "EROR"
+            case "else3":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="else4"
+                else:
+                    return "EROR"
+            case "else4":
+                if(Lexim[i]=="n"):
+                    i+=1
+                    case="else5"
+                else:
+                    return "EROR"
+            case "else5":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="else6"
+                else:
+                    return "EROR"
+            case "else6":
+                if(Lexim[i]=="m"):
+                    i+=1
+                    case="else7"
+                else:
+                    return "EROR"
+            case "else7":
+                if(Lexim[i]=="\n"):
+                    return "<ElseToken>"
+                else:
+                    return "EROR"
+            
+            # ----------------------------
+            
+            case "for1":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="for2"
+                else:
+                    return "EROR"
+            case "for2":
+                if(Lexim[i]=="m"):
+                    i+=1
+                    case="for3"
+                else:
+                    return "EROR"
+            case "for3":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="for4"
+                else:
+                    return "EROR"
+            case "for4":
+                if(Lexim[i]=="z"):
+                    i+=1
+                    case="for5"
+                else:
+                    return "EROR"
+            case "for5":
+                if(Lexim[i]=="\n"):
+                    return "<ForToken>"
+                else:
+                    return "EROR"
+            
+            # ----------------------------
+            
+            case "def1":
+                if(Lexim[i]=="s"):
+                    i+=1
+                    case="def2"
+                else:
+                    return "EROR"
+            case "def2":
+                if(Lexim[i]=="t"):
+                    i+=1
+                    case="def3"
+                else:
+                    return "EROR"
+            case "def3":
+                if(Lexim[i]=="o"):
+                    i+=1
+                    case="def4"
+                else:
+                    return "EROR"
+            case "def4":
+                if(Lexim[i]=="o"):
+                    i+=1
+                    case="def5"
+                else:
+                    return "EROR"
+            case "def5":
+                if(Lexim[i]=="r"):
+                    i+=1
+                    case="def6"
+                else:
+                    return "EROR"
+            case "def6":
+                if (Lexim[i]=='\n'):
+                    return "<CallToken>" 
+                elif(Lexim[i]=="a"):
+                    i+=1
+                    case="def7"
+                else:
+                    return "EROR"
+            case "def7":
+                if(Lexim[i]=="t"):
+                    i+=1
+                    case="def8"
+                else:
+                    return "EROR"
+            case "def8":
+                if(Lexim[i]=="_"):
+                    i+=1
+                    case="def9"
+                else:
+                    return "EROR"
+            case "def9":
+                if(Lexim[i]=="e"):
+                    i+=1
+                    case="def10"
+                else:
+                    return "EROR"
+            case "def10":
+                if(Lexim[i]=="l"):
+                    i+=1
+                    case="def11"
+                else:
+                    return "EROR"
+            case "def11":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="def12"
+                else:
+                    return "EROR"
+            case "def12":
+                if(Lexim[i]=="h"):
+                    i+=1
+                    case="def13"
+                else:
+                    return "EROR"
+            case "def13":
+                if(Lexim[i]=="i"):
+                    i+=1
+                    case="def14"
+                else:
+                    return "EROR"
+            case "def14":
+                if(Lexim[i]=="\n"):
+                    return"<DefToken>"
+                else:
+                    return "EROR"
+            
+            # ----------------------------
+            
+            case "input1":
+                if (Lexim[i]=="a"):
+                    i+=1
+                    case="input2"
+                else:
+                    return "EROR"
+            case "input2":
+                if (Lexim[i]=='\n'):
+                    return("<InputToken>")
+                else:
+                    return "EROR"
+            
+            # ----------------------------
+            
+            case "output1":
+                if(Lexim[i]=="j"):
+                    i+=1
+                    case="output2"
+                else:
+                    return "EROR"
+            case "output2":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="output2"
+                else:
+                    return "EROR"
+            case "output3":
+                if(Lexim[i]=="b"):
+                    i+=1
+                    case="output2"
+                else:
+                    return "EROR"
+            case "output4":
+                if(Lexim[i]=="a"):
+                    i+=1
+                    case="output2"
+                else:
+                    return "EROR"
+            case "output5":
+                if(Lexim[i]=="t"):
+                    i+=1
+                    case="output2"
+                else:
+                    return "EROR"
+            case "output6":
+                if(Lexim[i]=="\n"):
+                    return "<OutputToken>"
+                else:
+                    return "EROR"
+            
+            # ----------------------------
+            
         
         
         
